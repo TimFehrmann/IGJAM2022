d8c7a161d67c9c3f3df1e81cf7a21647a3c21ef7
Jothaka <jhkahle@hotmail.de>
2022-11-12 17:25:07 +0100

    InputProcessor

---
+++ Assets/Scripts/Utility/IInputProcessor.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public interface IInputProcessor
+{
+    Vector2 InputPosition();
+}
---
+++ Assets/Scripts/Utility/TouchInputProcessor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class TouchInputProcessor : IInputProcessor
+{
+    public Vector2 InputPosition()
+    {
+        return Input.touches[0].position;
+    }
+}
